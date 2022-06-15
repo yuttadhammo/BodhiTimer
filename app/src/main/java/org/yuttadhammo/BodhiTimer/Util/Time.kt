5a060ebe1e41fce69a3cbf2b039453a8a95a0cc7
tzugen <tzugen@riseup.net>
2022-06-15 21:29:24 +0200

    Time to Kotlin

--- app/src/main/java/org/yuttadhammo/BodhiTimer/Util/Time.kt
+++ app/src/main/java/org/yuttadhammo/BodhiTimer/Util/Time.kt
@@ -14,43 +14,33 @@
     You should have received a copy of the GNU General Public License
     along with Bodhi Timer.  If not, see <http://www.gnu.org/licenses/>.
 */
+package org.yuttadhammo.BodhiTimer.Util
 
-package org.yuttadhammo.BodhiTimer.Util;
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.yuttadhammo.BodhiTimer.R
+import java.util.regex.Pattern
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.text.TextUtils;
-import android.util.Log;
+object Time {
+    private const val TAG = "Time"
+    const val TIME_SEPARATOR = " again "
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import org.yuttadhammo.BodhiTimer.R;
-
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class Time {
-
-
-    private static final String TAG = "Time";
-
-    public static final String TIME_SEPARATOR = " again ";
-
-
-    public static int msFromNumbers(int hour, int minutes, int seconds) {
-        return hour * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000;
+    private fun msFromNumbers(hour: Int, minutes: Int, seconds: Int): Int {
+        return hour * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000
     }
 
-    public static int msFromArray(int[] numbers) {
-        return msFromNumbers(numbers[0], numbers[1], numbers[2]);
+    @JvmStatic
+    fun msFromArray(numbers: IntArray): Int {
+        return msFromNumbers(numbers[0], numbers[1], numbers[2])
     }
 
-    public static String padWithZeroes(int number) {
-        if (number > 9) {
-            return Integer.toString(number);
+    private fun padWithZeroes(number: Int): String {
+        return if (number > 9) {
+            number.toString()
         } else {
-            return "0" + number;
+            "0$number"
         }
     }
 
@@ -61,15 +51,18 @@ public class Time {
      * @param ms the time in milliseconds
      * @return the formatted string
      */
-    public static String ms2Str(int ms) {
-        int[] time = time2Array(ms);
-
-        if (time[0] == 0 && time[1] == 0) {
-            return Integer.toString(time[2]);
+    private fun ms2Str(ms: Int): String {
+        val time = time2Array(ms)
+        return if (time[0] == 0 && time[1] == 0) {
+            time[2].toString()
         } else if (time[0] == 0) {
-            return Integer.toString(time[1]).concat(":").concat(padWithZeroes(time[2]));
+            time[1].toString() + ":" + padWithZeroes(
+                time[2]
+            )
         } else {
-            return Integer.toString(time[0]).concat(":").concat(padWithZeroes(time[1])).concat(":").concat(padWithZeroes(time[2]));
+            time[0].toString() + ":" + padWithZeroes(
+                time[1]
+            ) + ":" + padWithZeroes(time[2])
         }
     }
 
@@ -79,114 +72,99 @@ public class Time {
      * @param time the time in milliseconds
      * @return [hour, minutes, seconds, ms]
      */
-    public static int[] time2Array(int time) {
-        int ms = time % 1000;
-        int seconds = time / 1000;  // 3550000 / 1000 = 3550
-        int minutes = seconds / 60; // 59.16666
-        int hours = minutes / 60; // 0.9
-
-        if (hours > 60)
-            hours = 60;
-
-        minutes = minutes % 60;
-        seconds = seconds % 60;
-
-        int[] timeVec = new int[4];
-        timeVec[0] = hours;
-        timeVec[1] = minutes;
-        timeVec[2] = seconds;
-        timeVec[3] = ms;
-        return timeVec;
+    @JvmStatic
+    fun time2Array(time: Int): IntArray {
+        val ms = time % 1000
+        var seconds = time / 1000 // 3550000 / 1000 = 3550
+        var minutes = seconds / 60 // 59.16666
+        var hours = minutes / 60 // 0.9
+        if (hours > 60) hours = 60
+        minutes %= 60
+        seconds %= 60
+        val timeVec = IntArray(4)
+        timeVec[0] = hours
+        timeVec[1] = minutes
+        timeVec[2] = seconds
+        timeVec[3] = ms
+        return timeVec
     }
 
-    public static String time2humanStr(Context context, int time) {
-        int[] timeVec = time2Array(time);
-        int hour = timeVec[0], minutes = timeVec[1], seconds = timeVec[2];
-
-        ArrayList<String> strList = new ArrayList<>();
-
-        Resources res = context.getResources();
+    @JvmStatic
+    fun time2humanStr(context: Context, time: Int): String {
+        val timeVec = time2Array(time)
+        val hour = timeVec[0]
+        val minutes = timeVec[1]
+        val seconds = timeVec[2]
+        val strList = ArrayList<String?>()
+        val res = context.resources
 
         // string formatting
         if (hour != 0) {
-            strList.add(res.getQuantityString(R.plurals.x_hours, hour, hour));
+            strList.add(res.getQuantityString(R.plurals.x_hours, hour, hour))
         }
         if (minutes != 0) {
-            strList.add(res.getQuantityString(R.plurals.x_mins, minutes, minutes));
+            strList.add(res.getQuantityString(R.plurals.x_mins, minutes, minutes))
         }
-        if (seconds != 0 || (seconds >= 0 && minutes == 0 && hour == 0)) {
-            strList.add(res.getQuantityString(R.plurals.x_secs, seconds, seconds));
+        if (seconds != 0 || seconds >= 0 && minutes == 0 && hour == 0) {
+            strList.add(res.getQuantityString(R.plurals.x_secs, seconds, seconds))
         }
-
-        return TextUtils.join(", ", strList);
+        return TextUtils.join(", ", strList)
     }
 
-
-    public static String time2hms(int time) {
-        return ms2Str(time);
+    fun time2hms(time: Int): String {
+        return ms2Str(time)
     }
 
-    public static String str2complexTimeString(AppCompatActivity activity, String numberString) {
-        String out;
-
-        ArrayList<String> stringArray = new ArrayList<>();
-
-
-        String[] strings = numberString.split(TIME_SEPARATOR);
-
-        for (String string : strings) {
-            int atime = str2timeString(activity, string);
-            if (atime > 0)
-                stringArray.add(atime + "#sys_def#" + activity.getString(R.string.sys_def));
+    @JvmStatic
+    fun str2complexTimeString(activity: AppCompatActivity, numberString: String): String {
+        val out: String
+        val stringArray = ArrayList<String?>()
+        val strings = numberString.split(TIME_SEPARATOR).toTypedArray()
+        for (string in strings) {
+            val atime = str2timeString(activity, string)
+            if (atime > 0) stringArray.add(atime.toString() + "#sys_def#" + activity.getString(R.string.sys_def))
         }
-
-        out = TextUtils.join("^", stringArray);
-
-        return out;
+        out = TextUtils.join("^", stringArray)
+        return out
     }
 
-    public static int str2timeString(AppCompatActivity activity, String numberString) {
-        final Resources res = activity.getResources();
-        final String[] numbers = res.getStringArray(R.array.numbers);
-        int position = 0;
-        String newString = numberString;
-        for (String number : numbers) {
-            int num = 60 - position++;
-            newString = newString.replaceAll(number, Integer.toString(num));
+    @JvmStatic
+    fun str2timeString(activity: AppCompatActivity, numberString: String): Int {
+        val res = activity.resources
+        val numbers = res.getStringArray(R.array.numbers)
+        var newString = numberString
+
+        for ((position, number) in numbers.withIndex()) {
+            val num = 60 - position
+            newString = newString.replace(number.toRegex(), num.toString())
         }
 
-        Pattern HOUR = Pattern.compile("([0-9]+) " + activity.getString(R.string.hour));
-        Pattern MINUTE = Pattern.compile("([0-9]+) " + activity.getString(R.string.minute));
-        Pattern SECOND = Pattern.compile("([0-9]+) " + activity.getString(R.string.second));
+        val HOUR = Pattern.compile("([0-9]+) " + activity.getString(R.string.hour))
+        val MINUTE = Pattern.compile("([0-9]+) " + activity.getString(R.string.minute))
+        val SECOND = Pattern.compile("([0-9]+) " + activity.getString(R.string.second))
 
-        int hours = 0;
-        int minutes = 0;
-        int seconds = 0;
-
-        Matcher m = HOUR.matcher(newString);
+        var hours = 0
+        var minutes = 0
+        var seconds = 0
+        var m = HOUR.matcher(newString)
         while (m.find()) {
-            String match = m.group(1);
-            hours += Integer.parseInt(match);
+            val match = m.group(1)
+            hours += match?.toInt() ?: 0
         }
-
-        m = MINUTE.matcher(newString);
+        m = MINUTE.matcher(newString)
         while (m.find()) {
-            String match = m.group(1);
-            minutes += Integer.parseInt(match);
+            val match = m.group(1)
+            minutes += match?.toInt() ?: 0
         }
-
-        m = SECOND.matcher(newString);
+        m = SECOND.matcher(newString)
         while (m.find()) {
-            String match = m.group(1);
-            seconds += Integer.parseInt(match);
+            val match = m.group(1)
+            seconds += match?.toInt() ?: 0
         }
-
-        Log.d(TAG, "Got numbers: " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds");
-
-        int total = hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000;
-        if (total > 60 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000)
-            total = 60 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000;
-        return total;
+        Log.d(TAG, "Got numbers: $hours hours, $minutes minutes, $seconds seconds")
+        var total = hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000
+        if (total > 60 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000) total =
+            60 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000
+        return total
     }
-
 }
