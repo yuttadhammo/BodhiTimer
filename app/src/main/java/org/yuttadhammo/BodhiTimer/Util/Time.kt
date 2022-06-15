70414323aa711cdda4ce29ca553c9e4552534ad8
tzugen <tzugen@riseup.net>
2022-06-15 22:22:58 +0200

    Update Copyright

--- app/src/main/java/org/yuttadhammo/BodhiTimer/Util/Time.kt
+++ app/src/main/java/org/yuttadhammo/BodhiTimer/Util/Time.kt
@@ -1,19 +1,10 @@
 /*
-    This file is part of Bodhi Timer.
+ * Time.kt
+ * Copyright (C) 2014-2022 BodhiTimer developers
+ *
+ * Distributed under terms of the GNU GPLv3 license.
+ */
 
-    Bodhi Timer is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Bodhi Timer is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Bodhi Timer.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package org.yuttadhammo.BodhiTimer.Util
 
 import android.content.Context
